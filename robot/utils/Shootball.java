59aaa56177b46b4fe0671e2320b63d8e2fe6082e
downsviewSS <downsviewSS@users.noreply.github.com>
2024-02-15 18:24:19 -0500

    Add files via upload

---
+++ robot/utils/Shootball.java
@@ -0,0 +1,41 @@
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
+
+// package frc.robot.utils;
+
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.Constants.Intakeconstants;
+// import frc.robot.subsystems.Shooter;
+
+// public class Shootball extends Command {
+//   /** Creates a new Shootball. */
+//   Shooter shooter;
+//   public Shootball(Shooter s) {
+//     shooter = s;
+//     addRequirements(shooter);
+//     // Use addRequirements() here to declare subsystem dependencies.
+//   }
+
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
+
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     shooter.shootball(Intakeconstants.shooter_speed);
+//   }
+
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {
+//     shooter.stop();
+//   }
+
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
